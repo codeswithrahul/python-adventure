62d056f4a69893cff50c17fe9b34b194c2111634
codeswithrahul <codeswithrahul@gmail.com>
2024-07-15 20:23:42 +0530

    Hello python

---
+++ Day1/first.py
@@ -0,0 +1,9 @@
+# python installation
+# python extenstion : autopep8 for formatiing and pylint and autoRun : code Runner
+
+print("hello python")
+print(5+5)
+print(4/2)
+
+# python  multiline comment
+'''this is not valid'''
