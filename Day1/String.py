3c4ea9d44c5b22c21020b936d47e8fa0cfde3069
Rahul Chouhan <78010526+codeswithrahul@users.noreply.github.com>
2024-07-19 17:32:20 +0000

    variable declaring and string method

---
+++ Day1/String.py
@@ -0,0 +1,20 @@
+# string in python
+# if we want to using multi line we can use tripe quote ''' your message/data'''
+name = "krishna"
+
+email = '''hi Krishna,
+
+I hope you are doing well. I wanted you inform you that...
+
+'''
+
+# print(email)
+
+# if we want to know the length
+# print(len(name))---> 7
+
+
+# slice method 
+# print(name[0:5]) -- krish
+# print(name[:2])  ----> kr
+# print(name[:]) ---> krishna
--- Day1/variable.py
+++ Day1/variable.py
@@ -1,1 +1,13 @@
 # variable
+# in python we can't add string and number , it will throw an error 
+# if we want to store boolean value first word should be capital True and False.
+#  It is case sensitive
+x = 10
+y = 5
+# print(x+y) ---> 15
+name = "rahul"
+x = 6
+# print(name + x)-----> error
+isActive = True
+isActive = False
+# print(isActive) ------> false
