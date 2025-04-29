3ecf2e443abb713dcf394d154704fb24d91dcb73
codeswithrahul <codeswithrahul@gmail.com>
2025-04-29 19:50:41 +0530

    while loop

---
+++ broCode/main.py
@@ -0,0 +1,70 @@
+# Madlibs game - This will be a word game where you will create story by filling in blanks with random words.
+
+# name = input("please tell me your name:")
+# birthplace = input ("tell me your birth place")
+
+# print(f"your name is {name} and your birth place is {birthplace} and you are very cute!!!!!!")
+
+
+# age is less than 18 --> you are not eligble
+# age is 18 to 50 --> you are eligble
+# age is more than 50 the -- > you are pro player
+
+
+# age= int(input("please enter your age : "))
+
+# if age > 50:
+#     print("you are pro player")
+# elif age <18:
+#     print("you are not eligible")
+# elif age == 18 or age <50:
+#     print("eligble")
+
+
+# python calculator game 
+
+# operator = input("please type these operato (%, + , - , /): ")
+# num1 = int(input("type first number :"))
+# num2 = int(input("type second number :"))
+
+# if (operator == "-"):
+#     print(num1 - num2)
+# elif(operator == "+"):
+#     print(f"total number is {num1 + num2}")
+
+# and so on ...
+# age = 18
+
+# print("you are pro player" if age > 50 else "no")
+
+#Validate userInput exercise
+# 1. UserName is not more thn 12 character 
+# 2. userName must not contain space
+# 3. userName must not contains digit
+
+# userInput = input("please write your username:")
+
+# findLen = len(userInput)
+# findSpace = userInput.find(' ')
+# findDigit = userInput.isalpha()
+
+# if findLen > 12:
+#     print("userName should not more than 12 chartace")
+# elif findSpace != -1:
+#     print('userName must not container sapce')
+# elif findDigit == False:
+#     print("userName must not contains digit")
+# else:
+#     print("welocme")
+
+
+# num = int(input("Enter a number b/w 1 to 10: "))
+
+# while  num < 1 or num > 10:
+#     int(input("Enter a number b/w 1 to 10: "))
+
+# print(f"{num}")
+    
+
+
+
---
+++ deepak/Day1/level1.py
@@ -0,0 +1,86 @@
+# 1. Write a program to Swap two Variables a and b (Swapping basically means interchanging)
+# a = 5
+# b = 6
+
+# def swap(a,b):
+#     c = a + b
+#     a = c - a
+#     b = c - b
+#     return a,b
+    
+# print(swap(a,b))
+
+# --------------------------------------------------------------------
+
+# 2. Write a program to print the largest of the three numbers.
+# a = 4
+# b = 61
+# c = 13
+
+# def largestThreeNumber(a,b,c):
+#     if a > b and a > c:
+#         return a
+#     elif b > c:
+#         return b
+#     else:
+#         return c
+
+# print(largestThreeNumber(a,b,c))
+
+# ---------------------------------------------------------------------------------
+
+
+#3. Write a program to assume a value of marks and print whether a person has failed or passed
+# If Marks >=40 --> pass
+
+# marks = 38
+
+# def failOrPass(m):
+#     if m>=40:
+#         print('pass')    # def swap(a,b):
+#     c = a + b
+#     a = c - a
+#     b = c - b
+#     return a,b
+#     else:
+#         print("fail")
+
+# print(failOrPass(marks))
+
+
+# -----------------------------------------------------------------------------------------------
+
+
+
+# 4. Design a program to assign grades to students based on the following criteria:
+
+#  - Students scoring less than or equal to 50 should be awarded a "D" grade.
+
+#  - Students scoring above 50 but below 60 should receive a "C" grade.
+
+#  - Students with marks between 60 to 75 should be assigned a "B" grade.
+
+#  - Students scoring greater than 75 should be granted an "A" grade.
+
+
+#  Example:
+
+#  Suppose the program is given the following student scores:
+
+#  45
+
+#  58
+
+#  70
+
+#  80
+
+#  The program should output the corresponding grades for each student:
+
+#  D
+
+#  C
+
+#  B
+
+#  A
